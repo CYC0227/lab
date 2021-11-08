eba121ab7012ba72043267a925880843082210c1
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-11-08 16:12:42 +0900

    ㅇㅇ

--- src/main/java/LabApplication2.java
+++ src/main/java/LabApplication2.java
@@ -1,8 +1,59 @@
+import lombok.Data;
 
+import java.util.Objects;
+
+@Data
+class Car implements Cloneable{
+    int a,b;
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Car car = (Car) o;
+//        return a == car.a && b == car.b;
+//    }
+
+    @Override
+    public boolean equals(Object o){
+        Car car = (Car) o;
+        if(this.a == car.a && this.b == car.b)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+
+}
 public class LabApplication2 {
-    public static void main(String[] args) {
-        for (int i = 0 ; i < 10; i++)
-            System.out.println(123);
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Car car = new Car();
+
+        car.setA(10);
+        car.setB(20);
+
+        Car car2 = (Car)car.clone();
+
+        System.out.println("car = " + car);
+        System.out.println("car2 = " + car2);
 
     }
 
